847b44cdf111ee85a8bf3fb3109cf834b31a7257
Josh Cross <crossjoshua6@gmail.com>
2017-12-05 14:56:52 +0000

    added an album class which requires the artists, and the name of the album.

--- Visual Studio 2015/Projects/Rythmm/Rythmm/Models/Album.cs
+++ Visual Studio 2015/Projects/Rythmm/Rythmm/Models/Album.cs
@@ -2,10 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace Rythmm.Models
 {
     public class Album
     {
+        //the iD of the Album.
+        public byte Id { get; set; }
+
+        //the name of the album is required.
+        [Required]
+        //the name of the album must be less than 100.
+        [StringLength(100)]
+        //the name of the album.
+        public string Name { get; set; }
+
+        //the artist which released the album.
+        public Artist Artist { get; set; }
     }
 }
