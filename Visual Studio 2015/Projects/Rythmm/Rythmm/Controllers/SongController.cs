6d7a14d9eaa76dbd17537afe7df8f868bc771a47
Josh Cross <crossjoshua6@gmail.com>
2017-12-05 16:35:39 +0000

    Added an index page for the songController which simply gets the songs from the database (empty at current time) and displays no songs if the IEnumerable is empty. Also created a new button which calls the new action which at the moment does nothing.

--- Visual Studio 2015/Projects/Rythmm/Rythmm/Controllers/SongController.cs
+++ Visual Studio 2015/Projects/Rythmm/Rythmm/Controllers/SongController.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+//NEED THIS FOR _CONTEXT!
+using Rythmm.Models;
 
 namespace Rythmm.Controllers
 {
     public class SongController : Controller
     {
+
+        private ApplicationDbContext _context;
+
+        //constructor.
+        public SongController()
+        {
+            _context = new ApplicationDbContext();
+        }
 
         //[Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
 
@@ -15,7 +26,23 @@ namespace Rythmm.Controllers
         [Route("Song")]
         public ActionResult Index()
         {
-            return Content("Test");
+            var songs = _context.Song.ToList();
+
+            return View(songs);
+        }
+
+        public ActionResult New()
+        {
+            //getting the genres from the database.
+            //var genres = _context.
+
+            return View("SongForm");
+        }
+
+        //disposing the _context.
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
         }
     }
 }
