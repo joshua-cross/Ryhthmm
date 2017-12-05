94be16f6a8be81a4b585ad85cfedbae6ac76bba5
Josh Cross <crossjoshua6@gmail.com>
2017-12-05 14:34:04 +0000

    Added a model for the song which contains details such as: a. the name of the song, b. the artist of the song, and c. the Genre of the song.

---
+++ Visual Studio 2015/Projects/Rythmm/Rythmm/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Rythmm.MvcApplication" Language="C#" %>
---
+++ Visual Studio 2015/Projects/Rythmm/Rythmm/Models/Song.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using Rythmm.Models;
+
+namespace Rythmm.Models
+{
+    public class Song
+    {
+        //The Name field will be required.
+        [Required]
+        //the maximum length this string can be is 255 characters.
+        [StringLength(255)]
+        //the name of the song
+        public string Name { get; set; }
+
+        //will be the primary key
+        public byte Id { get; set; }
+
+        //The artist of the song.
+        public Artist Artist { get; set; }
+
+        //the id of the artist.
+        public byte ArtistId { get; set; }
+
+        //the genre of the song.
+        public Genre Genre { get; set; }
+
+        //the id of the genre.
+        public byte GenreId { get; set; }
+
+        //the date in which the song was released.
+        public DateTime Released { get; set; }
+
+        public string Description { get; set; }
+    }
+}
