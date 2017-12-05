91e91214f8c6133c40b4c4cf18d748ce57da7ae4
Josh Cross <crossjoshua6@gmail.com>
2017-12-05 14:50:20 +0000

    a. Added a nationality model which contains the name and id of a nationality b. added an artist model which contains details like i. the name of the aritst ii. the nationality of the artist.

---
+++ Visual Studio 2015/Projects/Rythmm/Rythmm/Models/Nationality.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Rythmm.Models
+{
+    public class Nationality
+    {
+        //the id of the nationality (primary key)
+        public byte Id { get; set; }
+
+        //the name of the nationality is required.
+        [Required]
+        //the maximum length of this nationality is 50 characters
+        [StringLength(100)]
+        //the name of the nationality.
+        public string Name { get; set; }
+    }
+}
