e9701e749545632c0a83be465270a5741c882d2c
Josh Cross <crossjoshua6@gmail.com>
2017-12-06 12:34:48 +0000

    Added an artist controller as well as the _context, and a new aaction. Created the form for the artists now going to implement save button.

---
+++ Visual Studio 2015/Projects/Rythmm/Rythmm/Controllers/ArtistController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Rythmm.Models;
+using System.Data.Entity;
+using Rythmm.ViewModels;
+
+namespace Rythmm.Controllers
+{
+    public class ArtistController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        //constructor
+        public ArtistController ()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Artist
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Action that is called when we want to create a new artist.
+        public ActionResult New()
+        {
+            //the list of nationalities from the database.
+            var nationalities = _context.Nationalities.ToList();
+
+            //creating a viewModel for the ArtistForm page.
+            var viewModel = new ArtistFormViewModel()
+            {
+                Nationalities = nationalities,
+                Artist = new Artist()
+            };
+            //returning the ArtistForm view.
+            return View("ArtistForm", viewModel);
+        }
+
+        //disposing of the context.
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+    }
+}
