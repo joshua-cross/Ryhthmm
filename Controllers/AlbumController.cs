a4ae8f5ae27239d479f4774730c0b93e2dd738bf
Josh Cross <crossjoshua6@gmail.com>
2017-12-13 13:48:22 +0000

    Added a album controller which has a index action which just displays the albums in a table

---
+++ Controllers/AlbumController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Rythmm.Models;
+using System.Data.SqlClient;
+namespace Rythmm.Controllers
+{
+    public class AlbumController : Controller
+    {
+
+        private ApplicationDbContext _context;
+
+        //constructor
+        public AlbumController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Album
+        [Route("Album")]
+        public ActionResult Index()
+        {
+            var Albums = _context.Album.ToList();
+
+            return View("Index", Albums);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+    }
+}
